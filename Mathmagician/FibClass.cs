dcd43a4d847cb99b647510ae7c6a7b391739527e
sethdorris <sethc.dorris@gmail.com>
2015-10-25 14:36:26 -0500

    Created the fibonnaci numbers class and tests

---
+++ Mathmagician/FibClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class FibNumbers : NaturalNumbers
+    {
+        protected int next = 0;
+        public FibNumbers()
+        {
+            first = 1;
+            increment = 1;
+        }
+        public override int GetNext(int x)
+        {
+            next = (x - 1) + (x - 2);
+            return next;
+        }
+
+        public override int[] GetSequence()
+        {
+            return this.GetSequence(10);
+        }
+
+        public override int[] GetSequence(int x)
+        {
+            List<int> firstTen = new List<int> { };
+            int a = 0;
+            int b = 1;
+            for (int i = 1; i < x + 1; i++)
+            {
+                int item = a;
+                a = b;
+                b = item + b;
+                firstTen.Add(a);
+            }
+            return firstTen.ToArray();
+        }
+    }
+}
---
+++ MathmagicianTests/FibonnaciTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathmagician;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class FibonnaciTests
+    {
+        [TestMethod]
+        public void FibEnsureICanCreateClassInstance()
+        {
+            FibNumbers fib = new FibNumbers();
+            Assert.IsNotNull(fib);
+        }
+
+        [TestMethod]
+        public void FibShouldPrintFirstNumber()
+        {
+            FibNumbers fib = new FibNumbers();
+            int expected = 1;
+            int actual = fib.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibShouldPrintNextNumber()
+        {
+            FibNumbers fib = new FibNumbers();
+            int input = 4;
+            int expected = 5;
+            int actual = fib.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibShouldPrintSequenceOfNumbers()
+        {
+            FibNumbers fib = new FibNumbers();
+            int[] expected = new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+            int[] actual = fib.GetSequence();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibShouldPrintAVariableSequenceOfNumbers()
+        {
+            FibNumbers fib = new FibNumbers();
+            int input = 12;
+            int[] expected = new int[] {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+            int[] actual = fib.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibPrintSequenceOfNumbers()
+        {
+            FibNumbers fib = new FibNumbers();
+            int length = 7;
+            string expected = "1 1 2 3 5 8 13";
+            string actual = fib.ToString(fib.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
